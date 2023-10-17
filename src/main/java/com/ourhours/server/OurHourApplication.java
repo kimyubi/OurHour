56c95c9cf95edc0f4769667eed866649e05cabd2
kimyubi <ieieie0419@gmail.co,=m>
2023-10-17 19:03:37 +0900

    Init: 프로젝트 생성 .gitignore 작성

---
+++ src/main/java/com/ourhours/server/OurHourApplication.java
@@ -0,0 +1,13 @@
+package com.ourhours.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurHourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurHourApplication.class, args);
+	}
+
+}
